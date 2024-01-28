7b927f7dc40c6824bb3788bdea2f18ee274e7f39
Thijs Heerze <thijsheerze@gmail.com>
2024-01-28 16:31:47 +0100

    4. Rollensysteem functioneert niet

--- Bronbestanden/inloggen.php
+++ Bronbestanden/inloggen.php
@@ -5,29 +5,29 @@ $email = isset($_GET["Email"]) ? $_GET["Email"] : null;
 $password = isset($_GET["Wachtwoord"]) ? $_GET["Wachtwoord"] : null;
 
 if ($email !== null && $password !== null) {
-    $db = ConnectDB();
+     $db = ConnectDB();
 
-    $sql = "SELECT relaties.ID as RID, Wachtwoord, rollen.Waarde as Rol, Landingspagina 
-            FROM relaties
-            LEFT JOIN rollen ON relaties.FKrollenID = rollen.ID
-            WHERE Email = :email";
+     $sql = "SELECT relaties.ID as RID, Wachtwoord, rollen.Waarde as Rol, Landingspagina 
+          FROM relaties
+          LEFT JOIN rollen ON relaties.FKrollenID = rollen.ID
+          WHERE Email = :email";
 
-    $stmt = $db->prepare($sql);
-    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-    $stmt->execute();
+     $stmt = $db->prepare($sql);
+     $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+     $stmt->execute();
 
-    $inlog = $stmt->fetch();
+     $inlog = $stmt->fetch();
 
-    $redirect_url = 'index.php?NOAccount';
-    if ($inlog && password_verify($password, $inlog['Wachtwoord'])) {
-        $_SESSION['user'] = $inlog['RID'];
-        header("Location: " . $inlog['Landingspagina'] . '?RID=' . $inlog['RID']);
-        exit();
-    } else {
-        header("Location: index.php?NOAccount");
-        exit();
-    }
+     $redirect_url = 'index.php?NOAccount';
+     if ($inlog && password_verify($password, $inlog['Wachtwoord'])) {
+          $_SESSION['user'] = $inlog['RID'];
+          header("Location: " . $inlog['Landingspagina'] . '?RID=' . $inlog['RID']);
+          exit();
+     } else {
+          header("Location: index.php?NOAccount");
+          exit();
+     }
 } else {
-    echo 'Error: Missing parameters.';
+     echo 'Error: Missing parameters.';
 }
 ?>
